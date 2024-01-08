bf0fa6fc0c76ed58336393c756caf53e65188190
Owen-Sheasby <132654957+Owen-Sheasby@users.noreply.github.com>
2024-01-08 13:51:12 +0000

    added to front page js file

---
+++ frontpagejs.js
@@ -0,0 +1,7 @@
+
+function myFunction() {alert("you chose mental health");}
+function openProductivity() {location.href='productivity.html';}
+function openWealth() {location.href='wealth.html';}
+function openMentalHealth() {location.href='mentalhealth.html';}
+function openHealthNFitness() {location.href='healthandfitness.html';}
+function openMakingDecisions() {location.href='makingdecisions.html';}
